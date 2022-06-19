425cde54ada130aa0eefb074c10b362f3e5e8a69
Roman Nikulenkov <nrw@yandex.ru>
2022-06-19 21:49:37 +0300

    init

---
+++ eno.go
@@ -0,0 +1,37 @@
+// Package eno - общие коды ошибок
+package eno
+
+type ErrNo int
+
+const (
+	ErrNotImplemented   = iota + ErrNo(1) // не реализовано
+	ErrInternal                           // загадочная внутренняя ошибка
+	ErrNoAccess                           // нет доступа
+	ErrTooManyRequests                    // слишком много запросов
+	ErrNotAuthenticated                   // не залогинен
+	ErrObjectNotFound                     // объект не найден
+	ErrObjectExist                        // объект уже существует
+	ErrNotReady                           // операция не может быть выполнена, т.к. не готов сервер т.п.
+	ErrBadRequest                         // некорректный запрос
+	ErrDeadlineExceeded                   // закончилось отведенное время
+)
+
+var errDescr map[ErrNo]string = map[ErrNo]string{
+	ErrNotImplemented:   "ErrNotImplemented",
+	ErrInternal:         "ErrInternal",
+	ErrNoAccess:         "ErrNoAccess",
+	ErrTooManyRequests:  "ErrTooManyRequests",
+	ErrNotAuthenticated: "ErrNotAuthenticated",
+	ErrObjectNotFound:   "ErrObjectNotFound",
+	ErrObjectExist:      "ErrObjectExist",
+	ErrNotReady:         "ErrNotReady",
+	ErrBadRequest:       "ErrBadRequest",
+	ErrDeadlineExceeded: "ErrDeadlineExceeded",
+}
+
+func Name(e ErrNo) string {
+	if n, ok := errDescr[e]; ok {
+		return n
+	}
+	return ""
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/n-r-w/eno
+
+go 1.18
